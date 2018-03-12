8e9449f20e0181226920906ca9284ec9a863257b
Krishnakumar <krishnakumar@imperial.ac.uk>
2018-03-12 19:31:40 +0000

    This commit intends to fix pr 182 in lesson-example. Waiting for review

--- bin/lesson_initialize.py
+++ bin/lesson_initialize.py
@@ -64,7 +64,7 @@ and to meet some of our community members.
 ## Where to Contribute
 
 1.  If you wish to change this lesson,
-    please work in <https://github.com/swcarpentry/FIXME>,
+    please work in <https://github.com/carpentries/instructor-training>,
     which can be viewed at <https://swcarpentry.github.io/FIXME>.
 
 2.  If you wish to change the example lesson,
@@ -162,8 +162,8 @@ You can also [reach us by email][email].
 [github-flow]: https://guides.github.com/introduction/flow/
 [github-join]: https://github.com/join
 [how-contribute]: https://egghead.io/series/how-to-contribute-to-an-open-source-project-on-github
-[issues]: https://github.com/swcarpentry/FIXME/issues/
-[repo]: https://github.com/swcarpentry/FIXME/
+[issues]: https://github.com/carpentries/instructor-training/issues/
+[repo]: https://github.com/carpentries/instructor-training/
 [swc-issues]: https://github.com/issues?q=user%3Aswcarpentry
 [swc-lessons]: http://software-carpentry.org/lessons/
 [swc-site]: http://software-carpentry.org/
@@ -282,7 +282,7 @@ FIXME
 
 ROOT_AIO_MD = '''\
 ---
-layout: page 
+layout: page
 root: .
 ---
 <script>
